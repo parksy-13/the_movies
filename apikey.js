c860398c2fd7ee11e690c8fc68f1c359f282ed55
parksy-13 <141797627+parksy-13@users.noreply.github.com>
2024-01-09 17:40:06 +0900

    Delete apikey.js
    가리기 위해 삭제합니다

--- apikey.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMWVjZjliYjcxMjhjZGZlYWI2ODg2NmNkOTc5YjU2NSIsInN1YiI6IjY1OWE0Njc5MWQxYmY0MDIwMjNkNTc2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.e1y0Q0f89FWzHZbF-jZV8uShalbFnr4HKeKsJZSeGiE'
-    }
-  };
